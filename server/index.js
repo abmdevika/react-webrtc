44f2323930e207e8170c302ecdde9b96f2243f7a
devika <abmdevika@gmail.com>
2020-09-09 20:04:33 +0530

    heroku deploy related changes

--- server/index.js
+++ server/index.js
@@ -1,12 +1,18 @@
 const http = require('http');
+const path = require('path');
 const express = require('express');
 const config = require('../config');
 const socket = require('./lib/socket');
 
 const app = express();
 const server = http.createServer(app);
+const publicPath = path.join(__dirname, '..', 'public');
 
-app.use('/', express.static(`${__dirname}/../client`));
+app.use(express.static(publicPath));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(publicPath, 'index.html'));
+});
 
 server.listen(config.PORT, () => {
   socket(server);
