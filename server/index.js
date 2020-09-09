c36f6a324d694efa5a929d73f5d24911043e7a57
devika <abmdevika@gmail.com>
2020-09-09 21:03:31 +0530

    deploy issue

--- server/index.js
+++ server/index.js
@@ -6,9 +6,9 @@ const socket = require('./lib/socket');
 
 const app = express();
 const server = http.createServer(app);
-const publicPath = path.join(__dirname, 'build');
+const publicPath = path.join(__dirname, '../..', 'build');
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(publicPath));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(publicPath, 'index.html'));
